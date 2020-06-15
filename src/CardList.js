85caba9d61d3733df59901f0c3991158c7bde1cd
tpil <teopyl@gmail.com>
2020-06-15 15:13:13 +0300

    Card CardList and SearchBox components

---
+++ src/CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card.js';
+const CardList =({robots}) =>{
+ 
+
+    return(
+        <div>
+         {
+                robots.map((user, i) =>{
+                //return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+                return (
+                    <Card key={i} 
+                        id={user.id} 
+                        name={user.name} 
+                        email={user.email} 
+                        />
+                    );
+                    
+                  })
+        }
+        </div>
+
+    );
+}
+
+export default CardList;
---
+++ src/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBox = () => {
+
+    return(
+        <div >
+            <input type='search' className='pa3 ba b--green bg-lightest-blue' placeholder='search robots'/>
+        </div>
+    );
+}
+
+export default SearchBox;
