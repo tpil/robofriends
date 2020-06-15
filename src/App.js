ea19cb501edda817831b26a57c5e19d6508b3a79
tpil <teopyl@gmail.com>
2020-06-15 16:08:02 +0300

    App component coverted to class. onSearchChange added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';  //we need to destruct cause I didn't export default
 
 
-const App =()=>{
+class App extends React.Component{
+    constructor(){
+        super();//we need super to write about state
+        this.state={
+            robots: robots,
+            searchfield :''
+        }
+    }
+    //we create a trigger function for searchbox. We will pass it as argument from the app component
+    onSearchChange(event){
+        //event.target.value is the way to get the value of event
+        console.log(event.target.value);
+    }
+    render(){
+        return(
+            <div className='tc'>
+                <h1>Robo Friends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots={this.state.robots} />
+            </div>
+        );
 
-    return(
-        <div className='tc'>
-            <h1>Robo Friends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+    }
+   
 }
 
 export default App;
+
+//We use class components to pass props/states between 2 components through the parent Component 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const SearchBox = () => {
+//we can deconstruct props inside arrow function so we don't use props.searchChange
+//onChange is an html event in form elements
+const SearchBox = ({searchfield,searchChange}) => {
 
     return(
         <div >
-            <input type='search' className='pa3 ba b--green bg-lightest-blue' placeholder='search robots'/>
+            <input 
+                type='search' 
+                className='pa3 ba b--green bg-lightest-blue' 
+                placeholder='search robots'
+                onChange={searchChange} 
+            />
         </div>
     );
 }
 
 export default SearchBox;
+
+
