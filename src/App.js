27a94ece6abcce5b9da91173b5e781d6ec00f4cf
tpil <teopyl@gmail.com>
2020-06-15 16:45:07 +0300

    communicate searchfield with CardList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';  //we need to destruct cause I didn't export default
@@ -13,17 +12,24 @@ class App extends React.Component{
             searchfield :''
         }
     }
-    //we create a trigger function for searchbox. We will pass it as argument from the app component
-    onSearchChange(event){
-        //event.target.value is the way to get the value of event
-        console.log(event.target.value);
+    //******we create a trigger function for searchbox. We will pass it as argument from the app component
+    onSearchChange = (event) =>{
+        //console.log(event.target.value); event.target.value is the way to get the value of the object
+        this.setState({searchfield:event.target.value});
+      
     }
     render(){
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+
+        });
+        //console.log(filteredRobots);
+
         return(
             <div className='tc'>
                 <h1>Robo Friends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots={this.state.robots} />
+                <CardList robots={filteredRobots} />
             </div>
         );
 
@@ -33,4 +39,15 @@ class App extends React.Component{
 
 export default App;
 
+
+/*
+--------Guide---------
+1. we create smaller components first whhich we will use too frequently (Card)
+2.We put them in a list container CardList
+3.we Created the search input component to filter them
 //We use class components to pass props/states between 2 components through the parent Component 
+4. we create the class App component to communicate the 2 components
+5.We created the states and the event function to pass properties
+6. we change the state of searchfiled in the event function
+7.we pass the result as parametres to the components
+*/
