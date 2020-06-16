26c827be0c6261c51f4d1eb8fc1cd77cf1655211
tpil <teopyl@gmail.com>
2020-06-16 10:58:17 +0300

    added componentDidMount() method. Removed robot.js file

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends React.Component{
     constructor(){
         super();//we need super to write about state
         this.state={
-            robots: robots,
+            robots: [],
             searchfield :''
         }
     }
-    //******we create a trigger function for searchbox. We will pass it as argument from the app component
+
+    componentDidMount(){
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots:users}));
+    }
+
+    /*we create a trigger function for searchbox. We will pass it as argument from the app component
+    The trigger function MUST be EQUAL TO AND ARROW FUNCTION because when we pass the function as argument 
+    this.setState() can't be used in the searchBox functional component and gets error*/
     onSearchChange = (event) =>{
         //console.log(event.target.value); event.target.value is the way to get the value of the object
         this.setState({searchfield:event.target.value});
@@ -24,14 +34,17 @@ class App extends React.Component{
 
         });
         //console.log(filteredRobots);
-
-        return(
-            <div className='tc'>
-                <h1 className='f1'>Robo Friends</h1>
-                <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1>Loading ...</h1>
+        }else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>Robo Friends</h1>
+                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
 
     }
    
@@ -50,4 +63,7 @@ export default App;
 5.We created the states and the event function to pass properties
 6. we change the state of searchfiled in the event function
 7.we pass the result as parametres to the components
+8. We used react lifecycle methods to fetch() (it's a window.fetch() method which come to all browsers) 
+    some predefined users from a page (API). Convert to JSON file and setState robots with the new array. 
+    Removed the robots.js array.
 */
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card =(props)=>{
     const {name,email,id} = props;
     return(
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='photo' src={`https://robohash.org/${id}?200x200`} />
+            <img alt='Robot-pic' src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
